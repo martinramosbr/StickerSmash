f18ac62f418f726703f69a349d6dfaf9046fc9ef
Martinramos <91340006+martinramosbr@users.noreply.github.com>
2023-10-29 23:04:56 -0300

    adicionado diretorio de componentes e uso de bibliotecas expo

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Escolha uma foto"></Button>
+        <Button label="Use essa foto"></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
   },
 });
