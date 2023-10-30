42621c2953610cda099004e35b072a72c8ca02ca
Martinramos <91340006+martinramosbr@users.noreply.github.com>
2023-10-29 23:21:04 -0300

    adicionado a funcionalidade para escolher uma imagem da biblioteca do dispositivo.

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('Você não selecionou nenhuma imagem.');
+    }
+
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage}  selectedImage={selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Escolha uma foto"></Button>
+        <Button theme="primary" label="Escolha uma foto" onPress={pickImageAsync}></Button>
         <Button label="Use essa foto"></Button>
       </View>
       <StatusBar style="auto" />
